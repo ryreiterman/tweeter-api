99854714a29a610b55b098875f64372e8fa62621
Parker <phmd13@gmail.com>
2021-03-13 18:10:49 -0500

    commitment

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -4,10 +4,29 @@ class TweetsController < ApplicationController
         @tweets = Tweet.all 
         render json: @tweets
     end
+  
+    def show
+        tweet = Tweet.find(params[:id])
+        render(json: { tweet: tweet })
+    end
 
-   def show
-    tweet = Tweet.find(params[:id])
-    render(json: { tweet: tweet })
-   end
+    
+    def create
+    tweet = Tweet.new(tweet_params)
 
+    if tweet.save
+      render json: { tweet: tweet }
+    else
+      render(status: 422, json: { tweet: tweet, errors: tweet.errors })
+    end
+  end
+ 
+  
+private
+
+    def tweet_params
+    params.require(:title).permit(:content, :author)
+    end
 end
+
+  
